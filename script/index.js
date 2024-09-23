0c478495c62dfcd41e248848ccc695d3e92a5e78
lexerotk <lexerotk@gmail.com>
2024-09-23 23:54:30 +0300

    Optimize font size for mobile

--- script/index.js
+++ script/index.js
@@ -3,4 +3,9 @@ console.log("Mobile User: " + isMobile)
 if (isMobile==true) {
     document.getElementById("cont").classList.remove("container")
     document.getElementById("cont").classList.add("mcontainer")
+					
+					document.getElementById("hi").style.fontSize = "60px";
+					document.getElementById("text").style.fontSize = "15px";
+					document.getElementByClassName("title").style.fontSize = "35px";
+
 }
