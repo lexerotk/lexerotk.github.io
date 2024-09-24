3aeb08dc07e00de13977eaf69c05891e1d6675aa
lexerotk <lexerotk@gmail.com>
2024-09-24 18:48:16 +0300

    Fixed Bugs and optimized mobile experience (again)

--- script/index.js
+++ script/index.js
@@ -1,11 +1,14 @@
 const isMobile = navigator.userAgentData.mobile;
 console.log("Mobile User: " + isMobile)
+console.log(document.getElementsByClassName("text").length)
 if (isMobile==true) {
     document.getElementById("cont").classList.remove("container")
     document.getElementById("cont").classList.add("mcontainer")
-					
-					document.getElementById("hi").style.fontSize = "60px";
-					document.getElementById("text").style.fontSize = "15px";
-					document.getElementByClassName("title").style.fontSize = "35px";
-
+	for (let i = 0; i < document.getElementsByClassName("text").length; i++) {
+		document.getElementsByClassName("text")[i].style.fontSize = "15px";
+	}
+	for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
+		document.getElementsByClassName("title")[i].style.fontSize = "30px";
+	}
+	document.getElementById("hi").style.fontSize = "50px";
 }
