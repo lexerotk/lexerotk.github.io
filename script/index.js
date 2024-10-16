362cbaee4d5ebad7b909ffae53ed68f58c69c7f5
lexerotk <lexerotk@gmail.com>
2024-10-16 08:20:27 +0300

    Update index.js
    Make a responsive look for mobile.

--- script/index.js
+++ script/index.js
@@ -2,19 +2,14 @@ const isMobile = navigator.userAgentData.mobile;
 console.log("Mobile User: " + isMobile)
 console.log(document.getElementsByClassName("text").length)
 if (isMobile==true) {
-<<<<<<< HEAD
-=======
-    document.getElementById("cont").classList.remove("container")
-    document.getElementById("cont").classList.add("mcontainer")
->>>>>>> 3aeb08d (Fixed Bugs and optimized mobile experience (again))
 	for (let i = 0; i < document.getElementsByClassName("text").length; i++) {
 		document.getElementsByClassName("text")[i].style.fontSize = "15px";
 	}
 	for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
 		document.getElementsByClassName("title")[i].style.fontSize = "30px";
+ }
+  document.getElementById("container").style.width = "90%"
+	for (let i = 0; i < document.getElementsByClassName("navigationbutton").length; i++) {
+		document.getElementsByClassName("navigationbutton")[i].style.fontSize = "13px";
 	}
-<<<<<<< HEAD
-=======
-	document.getElementById("hi").style.fontSize = "50px";
->>>>>>> 3aeb08d (Fixed Bugs and optimized mobile experience (again))
 }
