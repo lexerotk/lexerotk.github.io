5f11fa9ca2bebd7dafd5ffbc976a1de54cf3ea5b
lexerotk <lexerotk@gmail.com>
2024-10-19 08:43:51 +0300

    Update Script File
    Gave it a nicer look.

--- script/index.js
+++ script/index.js
@@ -1,15 +1,21 @@
 const isMobile = navigator.userAgentData.mobile;
 console.log("Mobile User: " + isMobile)
-console.log(document.getElementsByClassName("text").length)
 if (isMobile==true) {
+	// TEXT SIZE
 	for (let i = 0; i < document.getElementsByClassName("text").length; i++) {
 		document.getElementsByClassName("text")[i].style.fontSize = "15px";
 	}
+
+	// TITLE SIZE
 	for (let i = 0; i < document.getElementsByClassName("title").length; i++) {
 		document.getElementsByClassName("title")[i].style.fontSize = "30px";
- }
-  document.getElementById("container").style.width = "90%"
+ 	}
+
+	// NAVIGATION BAR TEXT SIZE
 	for (let i = 0; i < document.getElementsByClassName("navigationbutton").length; i++) {
 		document.getElementsByClassName("navigationbutton")[i].style.fontSize = "13px";
 	}
+
+	// CONTAINER WIDTH
+	document.getElementById("container").style.width = "90%"
 }
