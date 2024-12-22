a81ea251a4c51c1f19ab279188e323a293f1ad96
lexerotk <lexerotk@gmail.com>
2024-12-22 03:40:21 +0300

    Archive old page and start from stratch

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
-const isMobile = navigator.userAgentData.mobile;
-console.log("Mobile User: " + isMobile)
-if (isMobile==true) {
-	document.getElementsByTagName("head")[0].innerHTML += '<link rel="stylesheet" href="/style/mobile.css">'
+let isMobile;
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+    isMobile = true;
 }
 else {
-	document.getElementsByTagName("head")[0].innerHTML += '<link rel="stylesheet" href="/style/desktop.css">'
+    isMobile = false;
+}
+console.log("LOG - Mobile User: " + isMobile)
+if (isMobile) {
+    document.getElementsByTagName("head")[0].innerHTML += '<link rel="stylesheet" href="/style/mobile.css">'
+}
+else {
+    document.getElementsByTagName("head")[0].innerHTML += '<link rel="stylesheet" href="/style/desktop.css">'
 }
