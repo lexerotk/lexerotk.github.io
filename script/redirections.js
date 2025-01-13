b269a32ff9b1b01934c312a33ceb58b1ea473ee0
lexerotk <lexerotk@gmail.com>
2025-01-14 02:23:50 +0300

    New design (mobile wip)

---
+++ script/redirections.js
@@ -0,0 +1,15 @@
+function home(){
+    location.href = "/index.html"
+}
+
+function about(){
+    location.href = "/about.html"
+}
+
+function thoughts(){
+    window.location.href = "/thoughts.html"
+}
+
+function moreaboutmyself(){
+    location.href = "/moreaboutmyself.html"
+}
