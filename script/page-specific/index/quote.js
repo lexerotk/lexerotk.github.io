1ee39da966881e527f7765c0a1b3fed330017048
lexerotk <lexerotk@gmail.com>
2025-04-06 00:15:33 +0300

    add a new quote

--- script/page-specific/index/quote.js
+++ script/page-specific/index/quote.js
@@ -7,10 +7,12 @@ const quotes  = [
     "What am I doing wrong?",
     "terrible creatures...",
     "I'm tired of this.",
-    "Nah. I'm fed up.",
-    "There is nothing I can do."
+    "I'm fed up.",
+    "There is nothing I can do.",
+    "Overthinking is a torture."
 ]
+
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
 console.log("LOG - Random quote for this page: "+ randomQuote)
-document.getElementById("quote").innerText = randomQuote;
+document.getElementById("quote").innerHTML = randomQuote;
