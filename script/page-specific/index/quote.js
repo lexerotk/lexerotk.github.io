cc1eaef84875e1e33a9c425f541ca6a22a1bf771
lexerotk <lexerotk@gmail.com>
2025-04-12 23:35:56 +0300

    update quotes

--- script/page-specific/index/quote.js
+++ script/page-specific/index/quote.js
@@ -3,13 +3,11 @@ const quotes  = [
     "To trust or not to trust.",
     "I'm sick of this.",
     "I was the worst punishment for myself! -Necip Fazıl Kısakürek",
-    "When is this gonna end..?",
     "What am I doing wrong?",
-    "terrible creatures...",
     "I'm tired of this.",
-    "I'm fed up.",
     "There is nothing I can do.",
-    "Overthinking is a torture."
+    "Overthinking is a torture.",
+    "Am I too naive..?"
 ]
 
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
