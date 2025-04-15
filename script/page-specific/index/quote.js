4193094d33000a85ae7a8770b6060bd95aaa0c7f
lexerotk <lexerotk@gmail.com>
2025-04-15 16:13:40 +0300

    Remove a quote

--- script/page-specific/index/quote.js
+++ script/page-specific/index/quote.js
@@ -1,6 +1,5 @@
 const quotes  = [
     "Embrace Darkness.",
-    "To trust or not to trust.",
     "I'm sick of this.",
     "I was the worst punishment for myself! -Necip Fazıl Kısakürek",
     "What am I doing wrong?",
