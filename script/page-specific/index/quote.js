48c5d60eaf4745a2121831f114ffbf4e2afcfe19
lexerotk <lexerotk@gmail.com>
2025-04-18 09:33:18 +0300

    Update quote.js

--- script/page-specific/index/quote.js
+++ script/page-specific/index/quote.js
@@ -1,16 +1,19 @@
 const quotes  = [
-    "Embrace Darkness.",
+//    "Embrace Darkness.",
     "I'm sick of this.",
-    "I was the worst punishment for myself! -Necip Fazıl Kısakürek",
+//    "I was the worst punishment for myself! -Necip Fazıl Kısakürek",
     "What am I doing wrong?",
     "I'm tired of this.",
     "There is nothing I can do.",
-    "Overthinking is a torture.",
-    "Am I too naive..?",
-    "Nobody",
-    "No one",
-    "Weirdo",
-    "Despair"
+//    "Overthinking is a torture.",
+//    "Am I too naive..?",
+//    "Nobody",
+//    "No one",
+//    "Weirdo",
+    "Despair",
+    "People are cruel.",
+    "Girls. They are just fucking terrible.",
+    "When you gonna stop stalking?"
 ]
 
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
