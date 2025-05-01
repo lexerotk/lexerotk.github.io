67036a252404b478ce3a55cdf6dee9da56c4b7e2
lexerotk <lexerotk@gmail.com>
2025-05-02 00:32:33 +0300

    update quotes

--- script/page-specific/index/quote.js
+++ script/page-specific/index/quote.js
@@ -1,9 +1,6 @@
 const quotes  = [
     "Embrace Darkness.",
-    "I love coding.",
-    "I don't regret anything I did.",
-    "A visitor???",
-    "Just call me lex or lexer."
+    "Removed proxy detection."
 ]
 
 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
