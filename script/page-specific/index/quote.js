0c75f6e43fc7cc27d56805e381eb4272a41e9792
lexerotk <lexerotk@gmail.com>
2025-05-03 16:13:24 +0300

    Move random quote loading code to index.html instead of having a seperate file

--- script/page-specific/index/quote.js
+++
@@ -1,9 +0,0 @@
-const quotes  = [
-    "Embrace Darkness.",
-    "Removed proxy detection."
-]
-
-let randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
-
-console.log("Random quote for this page: "+ randomQuote)
-document.getElementById("quote").innerHTML = randomQuote;
